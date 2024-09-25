99e1df31c7af29b6d74ecb823412c821aa5f07a3
Wajid <wajidshar60@gmail.com>
2024-09-25 11:52:30 +0500

    initial commit

---
+++ Socket.cs
@@ -0,0 +1,18 @@
+﻿using System.Net ;
+using System.Net.Sockets;
+using System.Security.Cryptography ; 
+
+namespace WebSockets.Socket {
+
+
+    class Socket {  
+
+         
+         static int Main(params string[] args ){    
+            
+            return  0  ; 
+         }
+        
+    }
+
+}
